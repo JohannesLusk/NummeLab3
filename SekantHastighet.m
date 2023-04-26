d9dac9e7eaf1cfd4ff0d60b99c8b3ccfeb13f9cf
noelPellkvist <128718845+noelPellkvist@users.noreply.github.com>
2023-04-26 13:16:25 +0200

    Hastighet sekant

---
+++ SekantHastighet.m
@@ -0,0 +1,13 @@
+function [V] = SekantHastighet(startVelocity1, startVelocity0)
+    %målet här är att hitta rätt hastighet för 3 grader
+
+     while abs(startVelocity1 - startVelocity0) > 5e-3
+
+        f0 = ComputeEulervsAngle(3, 1e-3, startVelocity0) - 1.83;
+        f1 = ComputeEulervsAngle(3, 1e-3, startVelocity1) - 1.83;
+        t_n = f1*((startVelocity1-startVelocity0)/(f1-f0));
+        startVelocity0 = startVelocity1;
+        startVelocity1 = startVelocity1-t_n;
+     end 
+     V = startVelocity1
+end
