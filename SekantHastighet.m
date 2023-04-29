114fca4d69ba07eedb89b45b661ab060e855e019
noelPellkvist <128718845+noelPellkvist@users.noreply.github.com>
2023-04-29 15:18:20 +0200

    Hastighet klar

--- SekantHastighet.m
+++ SekantHastighet.m
@@ -1,24 +1,27 @@
 function [V, E] = SekantHastighet(startVelocity0, startVelocity1, degree)
-   f0 = ComputeEulervsAngle(degree,1e-3,startVelocity0);
+format long
+ f0 = ComputeEulervsAngle(degree,1e-3,startVelocity0);
  f0 = f0-1.83;
  f1 = f0 + 1;
  etrunk = abs(f0-f1);
 
- while etrunk > 1e-6
+ while etrunk > 1e-3
       f1 = ComputeEulervsAngle(degree,1e-3,startVelocity1);
       f1 = f1 - 1.83;
      t_n = f1*((startVelocity1-startVelocity0)/(f1-f0));
      startVelocity0 = startVelocity1;
      startVelocity1 = startVelocity1-t_n;
      etrunk = abs(f0-f1)
-     if etrunk > 1e-6
+     if etrunk > 1e-3
         f0 = f1;
      end
  end
- x = -f0/(f1-f0);
+ f0 = f1 + 1.83
+ f1 = ComputeEulervsAngle(degree,1e-3,startVelocity1)
+ x = (1.83-f0)/(f1-f0);
  V = startVelocity0 + (startVelocity1-startVelocity0)*x;
- YLand = f0 + (f1-f0)*x + 1.83
+ %YLand = f0 + (f1-f0)*x + 1.83
 
- E = abs(startVelocity0-startVelocity1)
+ E = abs(startVelocity1-startVelocity0);
 end
 %vinkel, hastighet, 
