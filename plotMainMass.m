69cabb7e731292b3ae35a3163ce64fda971aaa24
Johannes lusk <lusk@kth.se>
2023-05-01 14:35:03 +0200

    la till massa och hastighets plots

---
+++ plotMainMass.m
@@ -0,0 +1,83 @@
+
+
+
+X0 = 0;
+Y0 = 1.84;
+degree = 3;
+theta = pi*degree/180;
+V0 =15;
+X_prim0 = V0 * cos(theta);
+Y_prim0 = V0 * sin(theta);
+Xend = 2.37;
+m = 20/1000; % mass in Kg
+step = 0.01;
+etrunk = 1;
+
+
+while abs(etrunk) > 1e-3 % error smaller than 1 mm, since all units are in meters
+    [X1Plot, Y1Plot] = EulerPlot(m, X0, Y0, X_prim0, Y_prim0, step,Xend);
+    [X2Plot, Y2Plot] = EulerPlot(m, X0, Y0, X_prim0, Y_prim0, step/2,Xend);
+    etrunk = Y1Plot(length(Y1Plot),2)-Y2Plot(length(Y2Plot),2);
+    step = step/2;
+end 
+
+
+
+
+
+plot(X2Plot(:,2),Y2Plot(:,2))
+axis([0 2.38 0 3]);
+xlabel("X");
+ylabel("Höjd");
+title("Kastbana")
+hold on
+
+
+X0 = 0;
+Y0 = 1.84;
+degree = 3;
+theta = pi*degree/180;
+V0 =15;
+X_prim0 = V0 * cos(theta);
+Y_prim0 = V0 * sin(theta);
+Xend = 2.37;
+m = 30/1000; % mass in Kg
+step = 0.01;
+etrunk = 1;
+
+
+while abs(etrunk) > 1e-3 % error smaller than 1 mm, since all units are in meters
+    [X1Plot, Y1Plot] = EulerPlot(m, X0, Y0, X_prim0, Y_prim0, step,Xend);
+    [X2Plot, Y2Plot] = EulerPlot(m, X0, Y0, X_prim0, Y_prim0, step/2,Xend);
+    etrunk = Y1Plot(length(Y1Plot),2)-Y2Plot(length(Y2Plot),2);
+    step = step/2;
+end 
+plot(X2Plot(:,2),Y2Plot(:,2))
+
+
+
+X0 = 0;
+Y0 = 1.84;
+degree = 3;
+theta = pi*degree/180;
+V0 =15;
+X_prim0 = V0 * cos(theta);
+Y_prim0 = V0 * sin(theta);
+Xend = 2.37;
+m = 40/1000; % mass in Kg
+step = 0.01;
+etrunk = 1;
+
+
+while abs(etrunk) > 1e-3 % error smaller than 1 mm, since all units are in meters
+    [X1Plot, Y1Plot] = EulerPlot(m, X0, Y0, X_prim0, Y_prim0, step,Xend);
+    [X2Plot, Y2Plot] = EulerPlot(m, X0, Y0, X_prim0, Y_prim0, step/2,Xend);
+    etrunk = Y1Plot(length(Y1Plot),2)-Y2Plot(length(Y2Plot),2);
+    step = step/2;
+end 
+plot(X2Plot(:,2),Y2Plot(:,2))
+
+legend({"kastbana vid 20 g","kastbana vid 30 gramm","Kastbana id 40 gramm"})
+
+
+
